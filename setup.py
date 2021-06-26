dbdf141badf79906fa59a3d390d6126340c4ef06
Chase Ruskin <c.ruskin@ufl.edu>
2021-06-25 22:34:32 -0400

    Updates project to be ready for packaging

--- setup.py
+++ setup.py
@@ -1,19 +1,9 @@
 import setuptools
-import os
-import pathlib
-#some setup code inspired by:
-# https://towardsdatascience.com/how-to-build-and-publish-command-line-applications-with-python-96065049abc1
+#code inspired by:
 # https://packaging.python.org/tutorials/packaging-projects/
 
 with open("README.md", "r", encoding="utf-8") as fh:
     entire_description = fh.read()
-
-with open("requirements.txt", 'r', encoding='utf-8') as f:
-    reqs = f.read().split('\n')
-
-install_list = [x.strip() for x in reqs if ('git+' not in x) \
-    and (not x.startswith('#') and (not x.startswith('-')))]
-dep_links = [x.strip().replace('git+', '') for x in reqs if 'git+' not in x]
 
 setuptools.setup(
     name="legoHDL",
@@ -22,22 +12,20 @@ setuptools.setup(
     author_email="c.ruskin@ufl.edu",
     description="A lightweight HDL package manager",
     long_description=entire_description,
-    long_descriotion_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     url="https://github.com/c-rus/legoHDL",
     classifiers=[
         "",
     ],
-    install_requires=install_list,
     entry_points='''
             [console_scripts]
             legoHDL=pkgmngr.manager:main
         ''',
-    dependency_links=dep_links,
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     include_package_data=True,
     package_data={
         "": ["settings.yml"],
     },
-    python_requires=">=3.6"
+    python_requires=">=3.5"
 )
